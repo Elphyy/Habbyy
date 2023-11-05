9a56b52b2906fba73df79047681ca134f5ac567d
lsign <lsign@proton.me>
2023-11-05 11:15:16 +0100

    add: database connection model

---
+++ models/db.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func Setup() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading.env file -->", err)
+		return
+	}
+
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	connection := fmt.Sprintf("host=localhost port=5432 user=%s password='%s' dbname=%s sslmode=disable", user, password, dbname)
+
+	db, err := sql.Open("postgres", connection)
+	if err != nil {
+		fmt.Println("Error connecting to database -->", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Error pinging database -->", err)
+		return
+	}
+
+	fmt.Println("Successfully connected to the database.")
+}
